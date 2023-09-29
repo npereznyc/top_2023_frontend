b4bae140b2bc594e669a0562f3028c9a04288555
npereznyc <natalieperez84@gmail.com>
2023-09-29 12:16:46 -0400

    Added Welcome page and folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Welcome from './pages/Welcome/Welcome';
 
 function App() {
   return (
     <div className="App">
+     
+      <Welcome />
       
     </div>
   );
