e9954c5f42c70bfe81edd0139edd6d3222cae415
Manny Sanchez <mannysanz715@gmail.com>
2023-10-04 18:14:28 -0700

    Style structure for Question Cards

--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -1,17 +1,41 @@
 import Button from "../Button/Button";
+import StatusBar from "../StatusBar/StatusBar";
+import { useState, useEffect } from "react";
 
-function QuestionCard({text1, text2, option1, option2, option3, option4}){
+function QuestionCard({text1, text2, option1, option2, option3, option4, popupPrompt, bannerImage}){
+  const [popUpText, setPopUpText] = useState(false)
+  useEffect(()=>{
+    if(!!popupPrompt){setPopUpText(true)}
+    else{setPopUpText(false)}
+  }, [])
+  
+
   return(
-    <>
-      <div>
-        <p>{text1}</p>
-        <p>{text2}</p>
-        <Button text={option1}/>
-        <Button text={option2}/>
-        <Button text={option3}/>
-        <Button text={option4}/>
+    <div className="flex items-center h-screen flex-col">
+        <StatusBar value={'100'}/>
+        <div className="flex flex-col pt-10 pb-20 gap-20 items-center h-full">
+        <img src={bannerImage} alt="compass"/>
+        <div>
+          <p>{text1}</p>
+          <p>{text2}</p>
+        </div>
+        <div>
+          <Button text={option1}/>
+          <Button text={option2}/>
+          <Button text={option3}/>
+          <Button text={option4}/>
+        </div>
+        <div className="flex flex-col justify-center items-center gap-10">
+          {popUpText && 
+            <div className="flex justify-center items-center gap-2">
+              <img src='/icon.png' width={20} style={{height: 20,}} alt="I icon" /> 
+              <p>{popupPrompt}</p>
+            </div>
+          }
+          <Button text={"Onward"} />
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
---
+++ src/components/StatusBar/StatusBar.jsx
@@ -0,0 +1,14 @@
+import './StatusBar.css'
+function StatusBar({value}){
+  return(
+    <>
+      <div className="w-4/5 h-8 px-1 flex items-center mb-4 bg-gray-200 rounded-full dark:bg-gray-700 mt-20">
+          <div className="h-5 px-3 bg-violet-600 rounded-full dark:bg-blue-500 w-5" style={{width: value + '%'}}>
+            <div className="lightColorHeight bg-violet-300 opacity-70 rounded-full " style={{width: '100%'}}></div>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default StatusBar;
