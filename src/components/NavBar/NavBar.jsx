d9b71a52359bc592419a73c84dfb8c7a4fecba50
npereznyc <natalieperez84@gmail.com>
2023-10-19 12:46:16 -0400

    Navbar opening and responding as desired.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,62 +1,59 @@
-function NavBar() {
+import './NavBar'
+
+function NavBar({ toggleSidebar, isSidebarOpen }) {
+
 
     return (
 
-        <nav class="bg-white border-gray-200 dark:bg-gray-900">
+        <nav className=" bg-white border-gray-200 dark:bg-gray-900 p-4">
+            {/* Logo */}
+            <div className=' flex justify-between mx-auto p-4'>
+            <a href="https://flowbite.com/" className="flex ">
+                
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Credit Hike</span>
+            </a>
 
-            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="https://flowbite.com/" class="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-                </a>
-                <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-                    <span class="sr-only">Open sidebar</span>
-                    <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-                    </svg>
-                </button>
-                {/* <div class="flex md:order-2">
-                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
-                    <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
-                        <span class="sr-only">Open main menu</span>
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                {/* Hamburger Menu */}
+                <div className='flex sm:hidden'>
+                    <button onClick={toggleSidebar} type="button" className={`flex self-end p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg ${isSidebarOpen ? 'hidden' : 'block'}`}>
+                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                         </svg>
                     </button>
-                </div> */}
-                <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-                    <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-                        <a href="https://flowbite.com/" class="flex items-center">
-                            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-                            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-                        </a>
-                        <ul class="space-y-2 font-medium">
-                            <li>
-                                <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-
-                                    <span class="ml-3">Dashboard</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-
-                                    <span class="flex-1 ml-3 whitespace-nowrap">Kanban</span>
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-
-                                    <span class="flex-1 ml-3 whitespace-nowrap">Users</span>
-                                </a>
-                            </li>
-
-                        </ul>
-                    </div>
-                </aside>
-
+                </div>
             </div>
-        </nav>
+            <div>
+            <aside className={`fixed top-0 left-0 z-40 bg-white transition-transform -translate-x-full sm:translate-x-0 ${isSidebarOpen ? 'w-screen h-screen translate-x-0' : '-translate-x-full'} sm:relative`} aria-label="Sidebar">
+                
+                <button onClick={toggleSidebar} type="button" className={`inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden ${isSidebarOpen ? 'block' : 'hidden'}`} >
+                    <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+                    </svg>
+                </button>
+                    <ul className="space-y-2 font-medium">
+                        <li>
+                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+
+                                <span className="ml-3">Dashboard</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+
+                                <span className="flex-1 ml-3 whitespace-nowrap">Kanban</span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+
+                                <span className="flex-1 ml-3 whitespace-nowrap">Users</span>
+                            </a>
+                        </li>
+                    </ul>
+            </aside>
+            </div>
+        </nav >
 
     )
 
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,8 +4,13 @@ import Landing from "../Landing/Landing";
 import Welcome from '../Welcome/Welcome'
 import NavBar from "../../components/NavBar/NavBar";
 
-function Main(){
+function Main() {
     const [currentStage, setCurrentStage] = useState('landing');
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
 
     const goToWelcome = () => setCurrentStage('welcome');
     const goToQuiz = () => setCurrentStage('quiz');
@@ -13,28 +18,31 @@ function Main(){
     return (
         <div>
             {currentStage === 'landing' && (
-                <Landing goToWelcome={goToWelcome}/>
+                <Landing goToWelcome={goToWelcome} />
             )}
 
             {currentStage === 'welcome' && (
-                <Welcome goToQuiz={goToQuiz}/>
+                <Welcome goToQuiz={goToQuiz} />
             )}
 
             {currentStage === 'quiz' && (
-                <>
-                <NavBar/>
-                <QuestionCard 
-                bannerImage={'/compass.png'} 
-                popupPrompt={"What's credit? And credit history?"} 
-                text1={'Complete the following sentence...'} 
-                text2={'My credit history is: '} 
-                option1={'Nonexistent'} 
-                option2={'Limited'} 
-                option3={'Moderate'} 
-                option4={'Extensive'} 
-                statusBarValue={'25'}/>
-                </>
-                
+                <div className='flex flex-col sm:flex-row'>
+                    <NavBar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
+                    <div className={`main-content flex-grow`}>
+                    <QuestionCard
+                        isSidebarOpen={isSidebarOpen}
+                        bannerImage={'/compass.png'}
+                        popupPrompt={"What's credit? And credit history?"}
+                        text1={'Complete the following sentence...'}
+                        text2={'My credit history is: '}
+                        option1={'Nonexistent'}
+                        option2={'Limited'}
+                        option3={'Moderate'}
+                        option4={'Extensive'}
+                        statusBarValue={'25'} />
+                        </div>
+                </div>
+
             )}
         </div>
     )
