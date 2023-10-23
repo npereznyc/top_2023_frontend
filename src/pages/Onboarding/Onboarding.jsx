646c9c8776307d128445b79634d64639c6a169bb
shaneyoung91 <shaneadam.young@gmail.com>
2023-10-23 16:13:28 -0700

    Learn More modal on Onboarding page

--- src/pages/Onboarding/Onboarding.jsx
+++ src/pages/Onboarding/Onboarding.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useState } from 'react';
+import { Modal } from 'flowbite-react';
 import './Onboarding.css';
 
 export default function Onboarding({goToQuiz}) {
+    
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+    
     return (
         <div className="flex flex-col pt-10 pb-20 gap-10 items-center h-full w-full">
             <div className='p1'>Along your hike, discover more with 'learn more' links.</div>
@@ -18,7 +31,7 @@ export default function Onboarding({goToQuiz}) {
                         fill="#6846BC"
                     />
                 </svg>
-                <span>Tap me to learn more</span>
+                <a href='#' onClick={openModal}>Tap me to learn more</a>
             </div>
             <div className='p3'>Visit the Glossary for quick term definitions</div>
             <div className='p4'>
@@ -36,8 +49,20 @@ export default function Onboarding({goToQuiz}) {
                 </svg>
                 <span>Tap to visit glossary</span>
             </div>
+
+            <Modal
+                show={isModalOpen}
+                onClose={closeModal}
+            >
+                <div className='custom-modal'>
+                    <Modal.Header className='modal-header'>
+                        <img src="/question-mark.png" alt="question-mark logo" />
+                        <span>Learn More</span>
+                    </Modal.Header>
+                    <Modal.Body className='modal-body'>These links will help deepen your learning along the way.</Modal.Body>
+                </div>
+            </Modal>
             <button onClick={goToQuiz} className="bg-green text-white py-2 px-4 rounded; button" ><span>Let's do this</span></button>
         </div>
     )
 }
-
